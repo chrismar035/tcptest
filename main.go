e14432b215db4c1d9902eda34d1886cfa807c306
Chris Marshall <chris@chrismar035.com>
2015-04-11 23:43:10 -0400

    Wrap an http server arount the tcp commands

--- main.go
+++ main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
+	"github.com/gorilla/mux"
 	"net"
+	"net/http"
 	"os"
 )
 
 func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/on", ledOn)
+	router.HandleFunc("/off", ledOff)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.Handle("/", router)
+	http.ListenAndServe(":"+port, nil)
+}
+
+func ledOn(res http.ResponseWriter, req *http.Request) {
+	sendCommand("led-on")
+
+}
+
+func ledOff(res http.ResponseWriter, req *http.Request) {
+	sendCommand("ledoff")
+}
+
+func sendCommand(command string) {
 	ip, port := "192.168.1.69", "333"
+	token := os.Getenv("HOMEBASE_TOKEN")
+	// ip, port := "10.0.1.215", "333"
 
 	tvAddr, err := net.ResolveTCPAddr("tcp", ip+":"+port)
 	if err != nil {
@@ -22,5 +48,5 @@ func main() {
 
 	var recv []byte
 	conn.Read(recv)
-	conn.Write([]byte("led-on\r\n"))
+	conn.Write([]byte(token + command + "\r\n"))
 }
